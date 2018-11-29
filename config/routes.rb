a4676c65802b0c986cad40ec7b818eeba7bd37c0
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-11-29 03:45:40 +0100

    pages controller + links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root 'items#index'
+
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
+
   resources :line_items, except: [:edit]
   resources :carts, path: :mycart, except: [:edit, :new]
   resources :items
   resources :charges
+
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/shop'
 end
